2ef468196f0d4bccfd06ca3e40dfd016af1a4389
Prabhu <iamprabhu@yahoo.com>
2014-02-26 20:22:52 +0530

    IndexedDB Code

---
+++ IndexedDB-A Persistent data store in the browser/store.js
@@ -0,0 +1,70 @@
+var healthyCodeSeminar;
+
+function createParticipantStore(){
+	var participant1 = {name:"Sam",company:"DuraSoft",designation:"Developer"};
+	var participant2 = {name:"Ram",company:"Yahoo",designation:"Architect"};
+
+	var request = indexedDB.open("HealthyCode Seminar",1);
+	request.onsuccess = function(){
+		healthyCodeSeminar = request.result;
+	}
+	request.onerror = function(event){
+		console.log("Error");
+		console.log(event);
+	}
+	request.onupgradeneeded = function(event){
+		 healthyCodeSeminar = event.target.result;
+		 healthyCodeSeminar.createObjectStore("ParticipantStore",{autoIncrement:true});
+	}		
+}
+
+function addParticipant(){
+	var txn = healthyCodeSeminar.transaction(["ParticipantStore"],"readwrite");
+	var participantStore = txn.objectStore("ParticipantStore");	
+	txn.oncomplete = function(event){
+		jQuery("#addmsg").text("Object added successfully");
+	}
+	txn.onerror = function(event){
+		jQuery("#addmsg").text("Error adding object");	
+		console.log("Error: " + event);
+	}
+	participantStore.add({
+		name:jQuery("#nametext").val(),
+		company:jQuery("#companytext").val(),
+		designation:jQuery("#designationtext").val()
+	});
+}
+
+function listParticipants(){
+	var txn = healthyCodeSeminar.transaction("ParticipantStore","readonly");
+	var participantStore = txn.objectStore("ParticipantStore");
+	jQuery("#participantslist").html("");
+	var cursorRequest = participantStore.openCursor();
+	cursorRequest.onsuccess = function(event){
+		var participantCursor = event.target.result;
+		if(participantCursor){
+			var li = "<li>";
+			li += participantCursor.key + ": " + 
+				participantCursor.value["name"] + ", " + 
+				participantCursor.value["company"] + ", " +
+				participantCursor.value["designation"];
+			li += "</li>";
+			jQuery("#participantslist").append(li);
+			participantCursor.continue();
+		}	
+	} 
+}
+
+function removeParticipant(){
+	var participantId = jQuery("#participantidtext").val();
+	var txn = healthyCodeSeminar.transaction("ParticipantStore","readwrite");
+	var participantStore = txn.objectStore("ParticipantStore");
+	var req = participantStore.delete(parseInt(participantId));
+	txn.oncomplete = function(event){
+		jQuery("#delmsg").text("Object deleted successfully");
+	}
+	txn.onerror = function(event){
+		console.log("Error deleting object");
+		console.log(event);
+	}
+}
